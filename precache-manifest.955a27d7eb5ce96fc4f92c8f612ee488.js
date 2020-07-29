246d22cbc8c69307563e8603dcde52f681e5daf4
Alexis Métaireau <alexis@notmyidea.org>
2020-07-29 12:23:34 +0200

    Update documentation

---
+++ precache-manifest.955a27d7eb5ce96fc4f92c8f612ee488.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c208349c69b1c440d1e76fc3b778ac6",
+    "url": "/bulma.css"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "379790011cb590d3cb6cf3664cee9fc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b2b598bc66ace74a3701",
+    "url": "/static/css/main.6c3ea5a1.chunk.css"
+  },
+  {
+    "revision": "b2b598bc66ace74a3701",
+    "url": "/static/js/main.b2b598bc.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "57d90eafe406586cac40",
+    "url": "/static/js/vendors~main.57d90eaf.chunk.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.75410995112df007b912d12f98f7571d.js"
+  "/precache-manifest.955a27d7eb5ce96fc4f92c8f612ee488.js"
 );
 
 workbox.core.skipWaiting();
