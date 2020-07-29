aa7164819339855f6ac8bf308311bb9e40c403f9
Alexis Métaireau <alexis@notmyidea.org>
2020-07-29 11:41:14 +0200

    Update documentation

---
+++ precache-manifest.eebc946135c55ec2fcc1dbe26d8d22b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c208349c69b1c440d1e76fc3b778ac6",
+    "url": "/commandes/bulma.css"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/commandes/favicon.ico"
+  },
+  {
+    "revision": "c1819bc8690f524ba00590617a30f0d3",
+    "url": "/commandes/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/commandes/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/commandes/manifest.json"
+  },
+  {
+    "revision": "a41afe4fb4dd61d1ce6c",
+    "url": "/commandes/static/css/main.6c3ea5a1.chunk.css"
+  },
+  {
+    "revision": "a41afe4fb4dd61d1ce6c",
+    "url": "/commandes/static/js/main.a41afe4f.chunk.js"
+  },
+  {
+    "revision": "5dfe485d72553c81ac8d",
+    "url": "/commandes/static/js/runtime~main.5dfe485d.js"
+  },
+  {
+    "revision": "57d90eafe406586cac40",
+    "url": "/commandes/static/js/vendors~main.57d90eaf.chunk.js"
+  }
+]);
